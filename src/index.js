e2c9a0919d4a0a7327aefde60a4d4d8c79564db1
Obaid Khan <softwareengineer0b101@gmail.com>
2020-10-31 12:48:08 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import bootstrap from "bootstrap";
+import Counter from "./components/counter";
+
+import App from "./components/App";
+
+ReactDOM.render(<Counter />, document.querySelector("#root"));
